f5ce27c74b7b3f9fe95c46ba159036dc889e06fb
endokagi <thewlip2008@gmail.com>
2020-05-25 14:54:26 +0700

    get all data & fix rating = null

--- sort_data.py
+++ sort_data.py
@@ -4,38 +4,45 @@ import csv
 neg_review = []
 pos_review = []
 
-place = 'patong_google'
-path = f'./data/{place}_clean_translated_add_rating.csv'
-with open(path, mode='r', encoding='utf-8') as read_obj:
-    csv_dict_reader = DictReader(read_obj)
-    for row in csv_dict_reader:
+def gen_output():
+    place = ['patong_google','patong_trip','promthep_google','promthep_trip','wat_google','wat_trip']
+    for data in place:
+        sort(data)
 
-        rate = row['Rating']
-        rating = int(rate)
-        Review = row['th']
+def sort(place):
+    path = f'./data/{place}_clean_translated_add_rating.csv'
+    with open(path, mode='r', encoding='utf-8') as read_obj:
+        csv_dict_reader = DictReader(read_obj)
+        for row in csv_dict_reader:
 
-        if rating == "null": 
-            neg_review.append(Review)
+            if row['rating'] in (None,'',""): rate = 0
+            else: rate = row['rating']
 
-        elif rating >= 0.0 and rating <= 2.5:
-            neg_review.append(Review)
+            rating = int(rate)
 
-        elif rating >= 2.6 and rating <= 5:
-            pos_review.append(Review)         
+            Review = row['th']
 
-fieldnames = ['Review']
-with open(f'./output/{place}_negative.csv', mode ='w', newline='', encoding='utf-8') as csvfile:
-                
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writeheader()
+            if rating >= 0.0 and rating <= 2.5:
+                neg_review.append(Review)
 
-                for data in neg_review:
-                    writer.writerow({'Review' : data})
+            elif rating >= 2.6 and rating <= 5:
+                pos_review.append(Review)         
 
-with open(f'./output/{place}_positive.csv', mode='w', newline='', encoding='utf-8') as csvfile:
+    fieldnames = ['Review']
+    with open(f'./output/{place}_negative.csv', mode ='w', newline='', encoding='utf-8') as csvfile:
+                    
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
 
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writeheader()
+                    for data in neg_review:
+                        writer.writerow({'Review' : data})
 
-                for data in pos_review:
-                    writer.writerow({'Review' : data})
+    with open(f'./output/{place}_positive.csv', mode='w', newline='', encoding='utf-8') as csvfile:
+
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+
+                    for data in pos_review:
+                        writer.writerow({'Review' : data})
+                        
+gen_output()
